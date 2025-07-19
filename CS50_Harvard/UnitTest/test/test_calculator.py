b8241bb5e591474ecf443a84d8d581b308c3de98
taiemotiv <tai@emotiv.com>
2025-07-19 18:21:01 +0700

    re organize folder

---
+++ CS50_Harvard/UnitTest/test/test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import square
+
+def test_square():
+    assert square(2) == 4
+def test_square_zero():
+    assert square(0) == 0
+# def main():
+#     test_square()
+
+# if __name__ == "__main__":
+#     main()
---
+++ CS50_Harvard/UnitTest/test/test_hello.py
@@ -0,0 +1,4 @@
+from helloworld import hello
+
+def test_defautl():
+    assert hello() == "hello, World"
