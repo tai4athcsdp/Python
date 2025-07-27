66f9cd4a05e9a866f3aae642c02c78d3fd5c0c2a
taiemotiv <tai@emotiv.com>
2025-07-27 20:36:35 +0700

    update unit test

--- CS50_Harvard/UnitTest/helloworld.py
+++ CS50_Harvard/UnitTest/helloworld.py
@@ -1,4 +1,12 @@
-x = float(input("What 's x?"))
-y = float(input("What 's y?"))
-z = round(x+y)
-print(f"{z:,}")
+# x = float(input("What 's x?"))
+# y = float(input("What 's y?"))
+# z = round(x+y)
+# print(f"{z:,}")
+
+def increase(n):
+    n+=1
+
+number = 100
+increase(number)
+print(number)
+
